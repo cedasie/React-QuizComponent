9be1a7975a12d8972a2cb56b8257c529a6599567
Cedasie <conquete.asie@gmail.com>
2019-02-22 20:25:00 +0100

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnwser: false };
+  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnwser: false });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnwser: true });
     }
   }
   render() {
@@ -29,6 +36,9 @@ class QuizQuestion extends Component {
               )}
             </ul>
           </section>
+          {this.state.incorrectAnwser ? (
+            <p className="error">Sorry, that's not right</p>
+          ) : null}
         </main>
       </div>
     );
